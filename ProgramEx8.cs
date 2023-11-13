ba908995ef83c1f39761d0ebbd06d7734fc66049
Guilherme Wilias <113528431+guilhermewilias@users.noreply.github.com>
2023-11-13 15:00:34 -0300

    Add files via upload

---
+++ ProgramEx8.cs
@@ -0,0 +1,38 @@
+﻿/*
+
+Escreva um programa que leia um número inteiro (variável CODIGO). 
+
+Verificar se o código é igual a 1, igual a 2 ou igual a 3. Caso não seja, apresentar a mensagem “Código inválido”. 
+
+Ao ser verificado o código e constatado que é um valor válido, o programa deve verificar cada código em separado para determinar seu valor por extenso, ou 
+seja, apresentar a mensagem “um”, ”dois” ou “três”. (Utilizar o comando Switch).
+
+*/
+
+using System.Net.Http.Headers;
+
+static void main()
+{
+    Console.Write("Insira um número inteiro --> ");
+    string variavelCodigo = Console.ReadLine()!;
+    int variavelCodigoInt = int.Parse(variavelCodigo);
+
+    switch (variavelCodigoInt)
+    {
+        case 1:
+            Console.WriteLine("\n\nUm");
+            break;
+        case 2: 
+            Console.WriteLine("\n\nDois");
+            break; 
+        case 3:
+            Console.WriteLine("\n\nTrês");
+            break;
+        default:
+            Console.WriteLine("\n\nCódigo inválido");
+            break;
+    }
+    
+}
+
+main();
